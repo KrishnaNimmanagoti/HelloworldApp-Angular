f6eb5ff56e40c6faaa115c6a5ab835f79eb4f154
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-05-11 08:33:17 +0530

    Added to Launch BridgLabz site in a new tab on clicking BridgeLabz Logo using Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HELLO-WORLD';
   imgUrl="../assets/BridgeLabz-Logo.jpg"
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
